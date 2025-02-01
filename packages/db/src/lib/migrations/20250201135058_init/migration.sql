d9e4bddbb79a312e5e32778fe12ab1fadb59b3f8
motechFR <mo@anyahq.co>
2025-02-01 14:51:28 +0100

    Split out farcaster user to its own identity

--- packages/db/src/lib/migrations/20250201135058_init/migration.sql
+++ packages/db/src/lib/migrations/20250201135058_init/migration.sql
@@ -3,8 +3,8 @@ CREATE TABLE "User" (
     "id" TEXT NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
-    "name" TEXT NOT NULL,
-    "farcasterId" INTEGER,
+    "path" TEXT NOT NULL,
+    "displayName" TEXT NOT NULL,
 
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
@@ -20,8 +20,28 @@ CREATE TABLE "GithubUser" (
     CONSTRAINT "GithubUser_pkey" PRIMARY KEY ("githubId")
 );
 
+-- CreateTable
+CREATE TABLE "FarcasterUser" (
+    "farcasterId" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+
+    CONSTRAINT "FarcasterUser_pkey" PRIMARY KEY ("farcasterId")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_path_key" ON "User"("path");
+
 -- CreateIndex
 CREATE UNIQUE INDEX "GithubUser_userId_key" ON "GithubUser"("userId");
 
+-- CreateIndex
+CREATE UNIQUE INDEX "FarcasterUser_userId_key" ON "FarcasterUser"("userId");
+
 -- AddForeignKey
 ALTER TABLE "GithubUser" ADD CONSTRAINT "GithubUser_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "FarcasterUser" ADD CONSTRAINT "FarcasterUser_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;

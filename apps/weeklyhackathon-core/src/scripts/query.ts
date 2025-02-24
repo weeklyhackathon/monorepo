6b547a62f4a2b195e9a948ba6ed27a2ff5595b81
jvaleskadevs <85419347+jvaleskadevs@users.noreply.github.com>
2025-02-24 22:32:36 +0100

    Fix Minor Issues (#26)

--- apps/weeklyhackathon-core/src/scripts/query.ts
+++ apps/weeklyhackathon-core/src/scripts/query.ts
@@ -16,4 +16,32 @@ async function script() {
   log.info(`Found ${prs.length} PRs`);
 }
 
-script();
+async function testFetchLastWeekPRs() {
+  const oneWeekAgo = new Date();
+  oneWeekAgo.setTime(oneWeekAgo.getTime() -  (7 * 60 * 60 * 24 * 1000 * 4));
+  
+  const prs = await prisma.githubPullRequest.findMany({
+      where: {
+        submittedAt: {
+          gte: oneWeekAgo
+        },
+        score: 0
+      }
+  });
+  log.info(`Found ${prs.length} PRs`);
+  log.info(prs);
+}
+
+async function testScore() {
+  const res = await prisma.githubPullRequest.update({
+    where: {
+      id: "3cd00033-a4d7-4af0-9bd0-d23a45775cc7"
+    },
+    data: {
+      score: 0
+    }
+  });
+  log.info(res);
+}
+
+//script();
